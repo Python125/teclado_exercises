9f6bb68a15785f8fc325238726b841cd53278dde
PythonMonty <91922582+Python125@users.noreply.github.com>
2024-03-28 15:30:22 -0400

    Create days_1_and_2.py

---
+++ days_1_and_2.py
@@ -0,0 +1,51 @@
+# Day 1
+
+# How old are you?
+# user responds with age
+# f"You are {age} years old"
+question = input("How old are you? ")
+print(f"You are {question} years old. ")
+
+# Number of days - 365
+# Number of weeks - 52
+# Number of months - 12
+# print("fThe number of years is {}")
+# Create variables for days, weeks, months
+# multiply days, weeks, months with perminater
+def b_day(x):
+    days = 365
+    weeks = 52
+    months = 12
+
+    print(f"In {x} years is {x * days} days, {x * weeks} weeks, and {x * months} months.")
+b_day(14)
+
+# Day 2
+
+question1 = input("What is your name? ")
+question2 = input("How old are you? ")
+print(f"Your name is {question1} and you are {question2} years old")
+
+# Below you’ll find some code with a number of errors.
+# Try to go through the program line by line and fix the issues in the code
+# In that exercise, it states that I need to correct any issues in the code below:
+
+"""
+hourly_wage = input("Please enter your hourly wage: ')
+
+prnt("Hourly wage: ")
+print(hourlywage)
+print("Hours worked: ")
+print(hours_worked)
+
+hours_worked = input("How many hours did you work this week? ")
+"""
+
+# Here is the corrected code:
+hourly_wage = input("Please enter your hourly wage: ")
+
+print("Hourly wage: ")
+print(hourly_wage)
+print("Hours worked: ")
+hours_worked = input("How many hours did you work this week? ")
+print(hours_worked)
