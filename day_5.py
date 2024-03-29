db601ea095ce28c3a452b06aee8a24296b97ea9d
PythonMonty <91922582+Python125@users.noreply.github.com>
2024-03-29 10:38:34 -0400

    Update day_5.py

--- day_5.py
+++ day_5.py
@@ -40,8 +40,21 @@ print(id(new_movies))
 
 print(movies == new_movies)
 print(movies is new_movies)
+print(movies)
+print(new_movies)
 
 tv = ["Father Knows Best", "Dragnet", "Dexter"]
 tv.append("Star Trek")
 
 print(id(tv))
+print(tv)
+
+# Ask the user to enter a number. Tell the user whether the number is positive, negative, or zero
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
